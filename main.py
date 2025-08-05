593b2f71619ade3f665079b2aa62fbfce76c54a1
Rohan <rohan1c47@gmail.com>
2025-08-05 19:37:01 +0530

    added a feature to save the details in a csv file

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-from selenium import webdriver # allow launching browser
-from selenium.webdriver.common.by import By # allow search with parameters
-from selenium.webdriver.support.ui import WebDriverWait # allow waiting for page to load
-from selenium.webdriver.support import expected_conditions as EC # determine whether the web page has loaded
-from selenium.common.exceptions import TimeoutException # handling timeout situation
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+import pandas as pd
+import time
 
-driver_option = webdriver.ChromeOptions()
-driver_option.add_argument(" — incognito")
-chromedriver_path = r"C:\Users\rohan\webscraping\chromedriver.exe" # Change this to your own chromedriver path!
-def create_webdriver():
-    service = Service(executable_path=chromedriver_path)
-    return webdriver.Chrome(service=service, options=driver_option)
+driver_option = webdriver.ChromeOptions()  #setting up for chrome
+driver_option.add_argument("--incognito")
 
-# Open the website
-browser = create_webdriver()
-browser.get("https://github.com/collections/machine-learning")
+chromedriver_path = r"C:\Users\rohan\webscraping\chromedriver.exe"     
+service = Service(executable_path=chromedriver_path)
+
+browser = webdriver.Chrome(service=service, options=driver_option)
+
+browser.get("https://github.com/collections/machine-learning")       #opening the page to scrape
+time.sleep(3)  # waiting for contents to load
+
+projects = browser.find_elements(By.CSS_SELECTOR, "h1.h3.lh-condensed")
+
+data = []
+for project in projects:
+    a_tag = project.find_element(By.TAG_NAME, "a")
+    name = a_tag.text
+    url = a_tag.get_attribute("href")
+    data.append({"Project Name": name, "URL": url})
+
+df = pd.DataFrame(data) #converting into dataframe 
+
+df.to_csv("github_ml_projects.csv", index=False)
+print("✅ Data saved to github_ml_projects.csv")
+
+browser.quit()
